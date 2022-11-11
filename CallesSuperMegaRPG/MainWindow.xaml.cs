35bfe1174349596a0722f6b236af8b3b98fbfaa0
SpookySnek <carleric.olsson@gmail.com>
2022-11-11 22:28:41 +0100

    Various bugfixes

--- CallesSuperMegaRPG/MainWindow.xaml.cs
+++ CallesSuperMegaRPG/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Engine.ViewModels;
 
-namespace CSMRPG_UI
+namespace CallesSuperMegaRPG
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
