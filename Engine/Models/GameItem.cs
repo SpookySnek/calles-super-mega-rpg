f488c61ee5328438ae85caf5edd3870821527996
SpookySnek <carleric.olsson@gmail.com>
2022-11-14 19:44:36 +0100

    Implemented ItemFactory

--- Engine/Models/GameItem.cs
+++ Engine/Models/GameItem.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    internal class GameItem
+    public class GameItem
     {
+        public int ItemTypeID { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public GameItem(int itemTypeID, string name, int price)
+        {
+            ItemTypeID = itemTypeID;
+            Name = name;
+            Price = price;
+        }
+        public GameItem Clone()
+        {
+            return new GameItem(ItemTypeID, Name, Price);
+        }
     }
 }
