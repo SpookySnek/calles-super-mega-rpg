cc2848e6827dd1f03062d524873b732fc5cfb960
SpookySnek <carleric.olsson@gmail.com>
2022-11-15 22:36:53 +0100

    Added Quest grid in MainWindow

--- Engine/Models/QuestStatus.cs
+++ Engine/Models/QuestStatus.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    internal class QuestStatus
+    public class QuestStatus
     {
+        public Quest PlayerQuest { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public QuestStatus(Quest quest)
+        {
+            PlayerQuest = quest;
+            IsCompleted = false;
+        }
     }
 }
