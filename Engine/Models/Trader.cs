bac03b765cd3d23b9c24daf2dd3cbd0517bea094
SpookySnek <carleric.olsson@gmail.com>
2022-12-02 13:09:22 +0100

    Added TraderFactory.cs

--- Engine/Models/Trader.cs
+++ Engine/Models/Trader.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Collections.ObjectModel;
 namespace Engine.Models
 {
-    internal class Trader
+    public class Trader : NotificationHandler
     {
+        public string Name { get; set; }
+        public ObservableCollection<GameItem> Inventory { get; set; }
+        public Trader(string name)
+        {
+            Name = name;
+            Inventory = new ObservableCollection<GameItem>();
+        }
+        public void AddItemToInventory(GameItem item)
+        {
+            Inventory.Add(item);
+        }
+        public void RemoveItemFromInventory(GameItem item)
+        {
+            Inventory.Remove(item);
+        }
     }
 }
