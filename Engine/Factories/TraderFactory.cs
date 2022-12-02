9f48b372521584b22a1ff96a55c4e8ca9fd55736
SpookySnek <carleric.olsson@gmail.com>
2022-12-02 13:18:49 +0100

    Added trader to Location.cs

--- Engine/Factories/TraderFactory.cs
+++ Engine/Factories/TraderFactory.cs
@@ -11,10 +11,13 @@ namespace Engine.Factories
         {
             Trader susan = new Trader("Susan");
             susan.AddItemToInventory(ItemFactory.CreateGameItem(1001));
+            
             Trader farmerTed = new Trader("Farmer Ted");
             farmerTed.AddItemToInventory(ItemFactory.CreateGameItem(1001));
+            
             Trader peteTheHerbalist = new Trader("Pete the Herbalist");
             peteTheHerbalist.AddItemToInventory(ItemFactory.CreateGameItem(1001));
+            
             AddTraderToList(susan);
             AddTraderToList(farmerTed);
             AddTraderToList(peteTheHerbalist);
