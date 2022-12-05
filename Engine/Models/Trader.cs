3f0331946249d9af6604bb2ca3bbc702bdcc3c12
SpookySnek <carleric.olsson@gmail.com>
2022-12-06 00:14:32 +0100

    Refactored models and added inheritence to LivingEntity

--- Engine/Models/Trader.cs
+++ Engine/Models/Trader.cs
@@ -1,22 +1,10 @@
-﻿using System.Collections.ObjectModel;
-namespace Engine.Models
+﻿namespace Engine.Models
 {
-    public class Trader : NotificationHandler
+    public class Trader : LivingEntity
     {
-        public string Name { get; set; }
-        public ObservableCollection<GameItem> Inventory { get; set; }
-        public Trader(string name)
+       public Trader(string name)
         {
             Name = name;
-            Inventory = new ObservableCollection<GameItem>();
-        }
-        public void AddItemToInventory(GameItem item)
-        {
-            Inventory.Add(item);
-        }
-        public void RemoveItemFromInventory(GameItem item)
-        {
-            Inventory.Remove(item);
         }
     }
 }
