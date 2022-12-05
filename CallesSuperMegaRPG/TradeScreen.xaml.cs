18288e5accc72922c36920c63887bd5b67a4eaec
SpookySnek <carleric.olsson@gmail.com>
2022-12-05 13:40:13 +0100

    Added basic logic for trade screen

--- CallesSuperMegaRPG/TradeScreen.xaml.cs
+++ CallesSuperMegaRPG/TradeScreen.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Engine.Models;
+using Engine.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,41 @@ namespace CallesSuperMegaRPG
     /// </summary>
     public partial class TradeScreen : Window
     {
+        public GameSession Session => DataContext as GameSession;
         public TradeScreen()
         {
             InitializeComponent();
+        }
+        private void OnClick_Sell(object sender, RoutedEventArgs e)
+        {
+            GameItem item = ((FrameworkElement)sender).DataContext as GameItem;
+            if (item != null)
+            {
+                Session.CurrentPlayer.Gold += item.Price;
+                Session.CurrentTrader.AddItemToInventory(item);
+                Session.CurrentPlayer.RemoveItemFromInventory(item);
+            }
+        }
+        private void OnClick_Buy(object sender, RoutedEventArgs e)
+        {
+            GameItem item = ((FrameworkElement)sender).DataContext as GameItem;
+            if (item != null)
+            {
+                if (Session.CurrentPlayer.Gold >= item.Price)
+                {
+                    Session.CurrentPlayer.Gold -= item.Price;
+                    Session.CurrentTrader.RemoveItemFromInventory(item);
+                    Session.CurrentPlayer.AddItemToInventory(item);
+                }
+                else
+                {
+                    MessageBox.Show("You do not have enough gold");
+                }
+            }
+        }
+        private void OnClick_Close(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
