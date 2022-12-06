551b71c82c2a6787c18354a578b0253b0e91ca3c
SpookySnek <carleric.olsson@gmail.com>
2022-12-06 15:35:13 +0100

    Addd new GroupedInventoryItem class, for grouping items

--- Engine/Models/Weapon.cs
+++ Engine/Models/Weapon.cs
@@ -5,7 +5,7 @@
         public int MinimumDamage { get; set; }
         public int MaximumDamage { get; set; }
         public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage)
-            : base(itemTypeID, name, price)
+            : base(itemTypeID, name, price, true)
         {
             MinimumDamage = minDamage;
             MaximumDamage = maxDamage;
