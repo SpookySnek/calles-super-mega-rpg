627a0a014bf5a64bbe536844847cfd01b3ff0672
SpookySnek <carleric.olsson@gmail.com>
2023-01-19 16:48:06 +0100

    Updated NotificationHandler to detect the property that called it

--- Engine/NotificationHandler.cs
+++ Engine/NotificationHandler.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Engine
 {
     public class NotificationHandler : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged(string propertyName)
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
