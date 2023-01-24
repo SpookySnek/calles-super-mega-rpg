96d33513ad3fe2d44ba5044d961ef38284f178ea
SpookySnek <carleric.olsson@gmail.com>
2023-01-24 19:32:02 +0100

    Refactored ItemFactory.cs to support the elemination of the Weapon class

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -8,31 +8,30 @@ namespace Engine.Factories
 
         static ItemFactory()
         {
-            _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
-            _standardGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 3));
-            _standardGameItems.Add(new GameItem(9001, "Snake fang", 1));
-            _standardGameItems.Add(new GameItem(9002, "Snake skin", 2));
-            _standardGameItems.Add(new GameItem(9003, "Rat tail", 1));
-            _standardGameItems.Add(new GameItem(9004, "Rat fur", 2));
-            _standardGameItems.Add(new GameItem(9005, "Spider fang", 1));
-            _standardGameItems.Add(new GameItem(9006, "Spider silk", 2));
-            _standardGameItems.Add(new GameItem(9007, "Snail shell", 2));
-            _standardGameItems.Add(new GameItem(9008, "Snail slime", 1));
+            BuildWeapon(1001, "Pointy Stick", 1, 1, 2);
+            BuildWeapon(1002, "Rusty Sword", 5, 1, 3);
+            
+            BuildMiscItem(9001, "Snake fang", 1);
+            BuildMiscItem(9002, "Snakeskin", 2);
+            BuildMiscItem(9003, "Rat tail", 1);
+            BuildMiscItem(9004, "Rat fur", 2);
+            BuildMiscItem(9005, "Spider fang", 1);
+            BuildMiscItem(9006, "Spider silk", 2); ;
         }
 
         public static GameItem CreateGameItem(int itemTypeID)
         {
-            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
-
-            if (standardItem != null)
-            {
-                if (standardItem is Weapon)
-                {
-                    return (standardItem as Weapon).Clone();
-                }
-                return standardItem.Clone();
-            }
-            return null;
+            return _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID)?.Clone();
+        }
+        private static void BuildMiscItem(int id, string name, int price)
+        {
+            _standardGameItems.Add(new GameItem(GameItem.ItemCategory.Miscellaneous, id, name, price));
+        }
+        private static void BuildWeapon(int id, string name, int price,
+                                        int minimumDamage, int maximumDamage)
+        {
+            _standardGameItems.Add(new GameItem(GameItem.ItemCategory.Weapon, id, name, price,
+                                                true, minimumDamage, maximumDamage));
         }
     }
 }
