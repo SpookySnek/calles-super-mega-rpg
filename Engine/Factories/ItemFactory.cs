4af4e21c9c835af89ce17892c3999164f408a735
SpookySnek <carleric.olsson@gmail.com>
2023-01-25 12:56:12 +0100

    Updated ItemFactory.cs to support new Actions

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -1,4 +1,5 @@
-﻿using Engine.Models;
+﻿using Engine.Actions;
+using Engine.Models;
 
 namespace Engine.Factories
 {
@@ -30,8 +31,11 @@ namespace Engine.Factories
         private static void BuildWeapon(int id, string name, int price,
                                         int minimumDamage, int maximumDamage)
         {
-            _standardGameItems.Add(new GameItem(GameItem.ItemCategory.Weapon, id, name, price,
-                                                true, minimumDamage, maximumDamage));
+            GameItem weapon = new GameItem(GameItem.ItemCategory.Weapon, id, name, price, true);
+            
+            weapon.Action = new AttackWithWeapon(weapon, minimumDamage, maximumDamage);
+            
+            _standardGameItems.Add(weapon);
         }
     }
 }
