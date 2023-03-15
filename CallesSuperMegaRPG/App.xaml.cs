7b8425e7b7f22f80d95ed70a40439cea6c32dca7
SpookySnek <carleric.olsson@gmail.com>
2023-03-15 13:35:53 +0100

    Implemented Exception Logging into view

--- CallesSuperMegaRPG/App.xaml.cs
+++ CallesSuperMegaRPG/App.xaml.cs
@@ -7,5 +7,13 @@ namespace CallesSuperMegaRPG
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            string exceptionMessageText =
+                $"An exception occurred: {e.Exception.Message}\r\n\r\nat: {e.Exception.StackTrace}";
+            LoggingService.Log(e.Exception);
+            // TODO: Create a Window to display the exception information.
+            MessageBox.Show(exceptionMessageText, "Unhandled Exception", MessageBoxButton.OK);
+        }
     }
 }
