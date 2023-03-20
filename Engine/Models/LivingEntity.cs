5ea3f4a8be0a2b3089688c03541ceb38313a8fd5
SpookySnek <carleric.olsson@gmail.com>
2023-03-20 12:19:06 +0100

    Updated LivingEntity to use the new Inventory functions

--- Engine/Models/LivingEntity.cs
+++ Engine/Models/LivingEntity.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.ObjectModel;
-
+﻿using System;
+using System.Collections.Generic;
+using Engine.Services;
 namespace Engine.Models
 {
     public abstract class LivingEntity : NotificationHandler
@@ -12,30 +13,28 @@ namespace Engine.Models
         private int _level;
         private GameItem _currentWeapon;
         private GameItem _currentConsumable;
-        
+        private Inventory _inventory;
         public string Name
         {
-            get => _name;
+            get { return _name; }
             private set
             {
                 _name = value;
                 OnPropertyChanged();
             }
         }
-        
         public int CurrentHitPoints
         {
-            get => _currentHitPoints;
+            get { return _currentHitPoints; }
             private set
             {
                 _currentHitPoints = value;
                 OnPropertyChanged();
             }
         }
-        
         public int MaximumHitPoints
         {
-            get => _maximumHitPoints;
+            get { return _maximumHitPoints; }
             protected set
             {
                 _maximumHitPoints = value;
@@ -44,45 +43,48 @@ namespace Engine.Models
         }
         public int Gold
         {
-            get => _gold;
+            get { return _gold; }
             private set
             {
                 _gold = value;
                 OnPropertyChanged();
             }
         }
-
         public int Level
         {
-            get => _level;
+            get { return _level; }
             protected set
             {
                 _level = value;
                 OnPropertyChanged();
             }
         }
-        
+        public Inventory Inventory
+        {
+            get => _inventory;
+            private set
+            {
+                _inventory = value;
+                OnPropertyChanged();
+            }
+        }
         public GameItem CurrentWeapon
         {
-            get => _currentWeapon;
+            get { return _currentWeapon; }
             set
             {
                 if (_currentWeapon != null)
                 {
                     _currentWeapon.Action.OnActionPerformed -= RaiseActionPerformedEvent;
                 }
-               
                 _currentWeapon = value;
-                
                 if (_currentWeapon != null)
                 {
                     _currentWeapon.Action.OnActionPerformed += RaiseActionPerformedEvent;
                 }
-
                 OnPropertyChanged();
             }
         }
-
         public GameItem CurrentConsumable
         {
             get => _currentConsumable;
@@ -92,56 +94,37 @@ namespace Engine.Models
                 {
                     _currentConsumable.Action.OnActionPerformed -= RaiseActionPerformedEvent;
                 }
-
                 _currentConsumable = value;
-
                 if (_currentConsumable != null)
                 {
                     _currentConsumable.Action.OnActionPerformed += RaiseActionPerformedEvent;
                 }
-
                 OnPropertyChanged();
             }
         }
-
-        public ObservableCollection<GameItem> Inventory { get; }
-        public ObservableCollection<GroupedInventoryItem> GroupedInventory { get; }
-        public List<GameItem> Weapons =>
-            Inventory.Where(i => i.Category == GameItem.ItemCategory.Weapon).ToList();
-        public List<GameItem> Consumables =>
-            Inventory.Where(i => i.Category == GameItem.ItemCategory.Consumable).ToList();
-
-        public bool HasConsumable => Consumables.Any();
-
         public bool IsDead => CurrentHitPoints <= 0;
-
         #endregion
-
         public event EventHandler<string> OnActionPerformed;
         public event EventHandler OnKilled;
-        protected LivingEntity(string name, int maximumHitPoints, int currentHitPoints, int gold, int level = 1)
+        protected LivingEntity(string name, int maximumHitPoints, int currentHitPoints,
+                               int gold, int level = 1)
         {
             Name = name;
             MaximumHitPoints = maximumHitPoints;
             CurrentHitPoints = currentHitPoints;
             Gold = gold;
             Level = level;
-            
-            Inventory = new ObservableCollection<GameItem>();
-            GroupedInventory = new ObservableCollection<GroupedInventoryItem>();
+            Inventory = new Inventory();
         }
-
         public void UseCurrentWeaponOn(LivingEntity target)
         {
             CurrentWeapon.PerformAction(this, target);
         }
-        
         public void UseCurrentConsumable()
         {
             CurrentConsumable.PerformAction(this, this);
             RemoveItemFromInventory(CurrentConsumable);
         }
-
         public void TakeDamage(int hitPointsOfDamage)
         {
             CurrentHitPoints -= hitPointsOfDamage;
@@ -177,81 +160,25 @@ namespace Engine.Models
         }
         public void AddItemToInventory(GameItem item)
         {
-            Inventory.Add(item);
-            if (item.IsUnique)
-            {
-                GroupedInventory.Add(new GroupedInventoryItem(item, 1));
-            }
-            else
-            {
-                if (!GroupedInventory.Any(gi => gi.Item.ItemTypeID == item.ItemTypeID))
-                {
-                    GroupedInventory.Add(new GroupedInventoryItem(item, 0));
-                }
-                GroupedInventory.First(gi => gi.Item.ItemTypeID == item.ItemTypeID).Quantity++;
-            }
-            OnPropertyChanged(nameof(Weapons));
-            OnPropertyChanged(nameof(Consumables));
-            OnPropertyChanged(nameof(HasConsumable));
+            Inventory = Inventory.AddItem(item);
         }
         public void RemoveItemFromInventory(GameItem item)
         {
-            Inventory.Remove(item);
-
-            GroupedInventoryItem groupedInventoryItemToRemove = item.IsUnique ?
-                GroupedInventory.FirstOrDefault(gi => gi.Item == item) :
-                GroupedInventory.FirstOrDefault(gi => gi.Item.ItemTypeID == item.ItemTypeID);
-
-            if (groupedInventoryItemToRemove != null)
-            {
-                if (groupedInventoryItemToRemove.Quantity == 1)
-                {
-                    GroupedInventory.Remove(groupedInventoryItemToRemove);
-                }
-                else
-                {
-                    groupedInventoryItemToRemove.Quantity--;
-                }
-            }
-            OnPropertyChanged(nameof(Weapons));
-            OnPropertyChanged(nameof(Consumables));
-            OnPropertyChanged(nameof(HasConsumable));
+            Inventory = Inventory.RemoveItem(item);
         }
-
-        public void RemoveItemsFromInventory(List<ItemQuantity> itemQuantities)
+        public void RemoveItemsFromInventory(IEnumerable<ItemQuantity> itemQuantities)
         {
-            foreach (ItemQuantity itemQuantity in itemQuantities)
-            {
-                for (int i = 0; i < itemQuantity.Quantity; i++)
-                {
-                    RemoveItemFromInventory(Inventory.First(item => item.ItemTypeID == itemQuantity.ItemID));
-                }
-            }
+            Inventory = Inventory.RemoveItems(itemQuantities);
         }
-
-        public bool HasAllTheseItems(List<ItemQuantity> items)
-        {
-            foreach (ItemQuantity item in items)
-            {
-                if (Inventory.Count(i => i.ItemTypeID == item.ItemID) < item.Quantity)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
         #region Private functions
         private void RaiseOnKilledEvent()
         {
             OnKilled?.Invoke(this, new System.EventArgs());
         }
-
         private void RaiseActionPerformedEvent(object sender, string result)
         {
             OnActionPerformed?.Invoke(this, result);
         }
-        
         #endregion
     }
 }
