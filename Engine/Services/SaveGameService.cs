8c3a4d43f13faf94d6549812c4d424f91093c52b
SpookySnek <carleric.olsson@gmail.com>
2023-03-31 12:14:35 +0200

    Started creating new Save Game service

---
+++ Engine/Services/SaveGameService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Engine.Services
+{
+    internal class SaveGameService
+    {
+    }
+}
